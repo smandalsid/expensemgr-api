03450a754b1f42dc2d49f3d9d8fd380e6d2cd413
smandalsid <smandal.sid@gmail.com>
2026-05-05 14:45:35 +0530

    chore: add change password endpoint and add env based auth

--- expensemgr/routers/users.py
+++ expensemgr/routers/users.py
@@ -6,6 +6,8 @@ from expensemgr.database.db import db_dependency
 from expensemgr.schemas.users import UserOut
 from expensemgr.services.auth import AuthService
 from expensemgr.services.users import UserService
+from expensemgr.utils.constants import auth_failed
+from expensemgr.utils.logger import expense_mgr_logger
 
 router = APIRouter(
     prefix="/users",
@@ -16,10 +18,11 @@ user_dependency = Annotated[dict, Depends(AuthService.get_current_user)]
 
 
 @router.get("/", status_code=status.HTTP_200_OK, response_model=UserOut)
+@expense_mgr_logger.wrapper_logger()
 def get_user(user: user_dependency, db: db_dependency):
     if user is None:
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication failed"
+            status_code=status.HTTP_401_UNAUTHORIZED, detail=auth_failed
         )
 
     user_model = UserService(db=db).get_user(user=user)
@@ -28,4 +31,26 @@ def get_user(user: user_dependency, db: db_dependency):
     else:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="User details not found"
+        )
+
+@router.put('/change_password', status_code=status.HTTP_200_OK)
+@expense_mgr_logger.wrapper_logger()
+def change_password(user: user_dependency, db: db_dependency, old_password: str, new_password: str, reenter_password: str):
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail=auth_failed
+        )
+    if new_password != reenter_password:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Passwords do not match!"
+        )
+    try:
+        return UserService(db=db).change_password(
+            user=user,
+            old_password=old_password,
+            new_password=new_password
+        )
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
         )
